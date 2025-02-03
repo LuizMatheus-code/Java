ce90183f3b7db0420107ef946945ba167d212f69
Luiz Matheus <84048511+LuizMatheus-code@users.noreply.github.com>
2025-02-03 09:59:20 -0300

    Update ExercicioExtra.java

--- Treina Recife/Java Basico/src/ExercicioExtra.java
+++ Treina Recife/Java Basico/src/ExercicioExtra.java
@@ -2,14 +2,14 @@ import java.util.Scanner;
 
 public class golsDeTimes {
     public static void main(String[] args) {
-        String[] times = new String[4];
-        int[] gols = new int[4];
+        String[] times = new String[2];
+        int[] gols = new int[2];
         int total_de_gols = 0;
         float media;
 
         Scanner entrada = new Scanner(System.in);
 
-        for (int contador = 0; contador < 4; contador++) {
+        for (int contador = 0; contador < 2; contador++) {
             System.out.println("Escreva o nome do time " + (contador + 1) + ": ");
             times[contador] = entrada.next();
 
@@ -19,13 +19,21 @@ public class golsDeTimes {
             total_de_gols += gols[contador];
         }
 
-        media = (float) total_de_gols / 4;
+        media = (float) total_de_gols / 2;
 
-        for (int i = 0; i < 4; i++) {
+        System.out.println("A média total de gols foi " + media + "\n");
+
+        for (int i = 0; i < 2; i++) {
             System.out.println("O time " + times[i] + " marcou " + gols[i] + " gol(s).");
+            
+            if (gols[i] > media) {
+                System.out.println("O time " + times[i] + " está acima da média.Foi Ótimo\n");
+            } else if (gols[i] < media) {
+                System.out.println("O time " + times[i] + " está abaixo da média. Foi Péssimo\n");
+            } else {
+                System.out.println("O time " + times[i] + " está na média. Foi Bom\n");
+            }
         }
-
-        System.out.println("\nA média total de gols foi " + media);
 
     }
 }
